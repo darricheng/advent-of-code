7a9865843d5f32b054246694d7d0dfd4ae899241
Darric Heng <darricheng@gmail.com>
2024-12-03 18:47:53 +0800

    update template to use module (so that we can def functions)

--- 2024/template.exs
+++ 2024/template.exs
@@ -1,15 +1,21 @@
-IO.puts("Running [[DAY]]-[[PART]]")
+defmodule AdventOfCode.Day[[DAY]]_[[PART]] do
+  def run do
+    IO.puts("Running [[DAY]]-[[PART]]")
 
-path = "./day[[DAY]]/data.txt"
+    path = "./day[[DAY]]/data.txt"
 
-data =
-  case File.read(path) do
-    {:ok, data} ->
-      data
+    data =
+      case File.read(path) do
+        {:ok, data} ->
+          data
 
-    {:error, reason} ->
-      IO.puts("File read failed. Reason: #{reason}")
-      System.halt(0)
+        {:error, reason} ->
+          IO.puts("File read failed. Reason: #{reason}")
+          System.halt(0)
+      end
+
+    data |> IO.inspect()
   end
+end
 
-data |> IO.inspect()
+AdventOfCode.Day[[DAY]]_[[PART]].run()
