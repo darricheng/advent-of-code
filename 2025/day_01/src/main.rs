3412a6977c56d7d76c40df73186e341b57848b40
Darric Heng <darricheng@gmail.com>
2025-12-01 18:35:57 +0800

    solved 1-2

--- 2025/day_01/src/main.rs
+++ 2025/day_01/src/main.rs
@@ -5,7 +5,7 @@ struct Dial {
     // Current value
     inner: i32,
     // Number of times the value is 0
-    zero_counter: u32,
+    zero_counter: i32,
 }
 
 impl Dial {
@@ -16,6 +16,8 @@ impl Dial {
         }
     }
     fn rotate(self: &mut Self, instruction: &str) {
+        let initial_value = self.inner;
+
         let (direction, clicks_str) = instruction.split_at(1);
         let clicks = clicks_str.parse::<i32>().unwrap();
 
@@ -24,14 +26,51 @@ impl Dial {
             "R" => self.inner + clicks,
             _ => panic!("direction wasn't L or R"),
         };
-        self.inner = new_value % 100;
-        if self.inner < 0 {
-            self.inner += 100;
-        }
 
-        if self.inner == 0 {
-            self.zero_counter += 1;
-        }
+        // 1-1
+        //
+        // self.inner = new_value % 100;
+        // if self.inner < 0 {
+        //     self.inner += 100;
+        // }
+        //
+        // if self.inner == 0 {
+        //     self.zero_counter += 1;
+        // }
+
+        // 1-2
+        let (additional_zeroes, new_inner) = match new_value {
+            v if v == 0 => (1, 0),
+            v if v > 0 => (v / 100, v % 100),
+            v if v < 0 => {
+                // Value is -ve, so we need to turn it +ve
+                let mut zeroes = v / 100 * -1;
+
+                // If we start at 0, we didn't go past zero the first time
+                if initial_value != 0 {
+                    zeroes += 1;
+                }
+
+                let mut remainder = v % 100;
+
+                // We only need to reset the range if the remainder is not 0
+                if remainder != 0 {
+                    remainder += 100
+                }
+
+                (zeroes, remainder)
+            }
+            _ => panic!("shouldn't happen as all instances covered"),
+        };
+
+        // Debugging purposes
+        // println!(
+        //     "instruction: {}, additional_zeroes: {}, new_inner: {}",
+        //     instruction, additional_zeroes, new_inner
+        // );
+
+        self.inner = new_inner;
+        self.zero_counter += additional_zeroes;
     }
 }
 
