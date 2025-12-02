fc87d5bb99af9cc923a1dcd0ff4605ab01f00360
Darric Heng <darricheng@gmail.com>
2025-12-02 18:24:44 +0800

    works with 2-2 sample data

--- 2025/day_02/src/main.rs
+++ 2025/day_02/src/main.rs
@@ -23,14 +23,49 @@ fn main() {
             let i_str = i.to_string();
 
             let i_str_len = i_str.len();
-            let value_to_add = if i_str_len % 2 == 0 {
-                let mid = i_str_len / 2;
-                let (first_half, second_half) = i_str.split_at(mid);
 
-                if first_half == second_half { i } else { 0 }
-            } else {
+            // 2-1
+            // let value_to_add = if i_str_len % 2 == 0 {
+            //     let mid = i_str_len / 2;
+            //     let (first_half, second_half) = i_str.split_at(mid);
+            //
+            //     if first_half == second_half { i } else { 0 }
+            // } else {
+            //     0
+            // };
+
+            // 2-2
+            let value_to_add = if i_str_len <= 1 {
                 0
+            } else {
+                // Convert to bytes slice so that it's easier to work with.
+                let bytes = i_str.as_bytes();
+
+                // Get the factors, which are the ways to to split the slice equally.
+                let chunk_sizes = find_factors(bytes.len());
+
+                // The resulting byte slices implement Eq, so we should be able to
+                // compare them directly. If they are all the same, we add this number
+                // to the accumulator and skip all the other factors.
+                let meets_criteria = chunk_sizes.into_iter().any(|size| {
+                    let mut chunks = bytes.chunks(size);
+
+                    let first_chunk = chunks.next().expect("Should have the first chunk");
+
+                    println!("first_chunk: {:?}", first_chunk);
+
+                    chunks.all(|chunk| {
+                        println!("chunk: {:?}", chunk);
+                        chunk == first_chunk
+                    })
+                });
+
+                println!("meets_criteria: {:?}", meets_criteria);
+
+                if meets_criteria { i } else { 0 }
             };
+
+            println!("value_to_add: {}", value_to_add);
 
             acc + value_to_add
         });
@@ -39,4 +74,28 @@ fn main() {
     });
 
     println!("Final: {}", final_value);
+}
+
+fn find_factors(number: usize) -> Vec<usize> {
+    // The factors are the possible chunk sizes.
+    let mut factors = vec![1];
+
+    // Floored sqrt returned by isqrt is the max that a factor can be
+    // We add one because we want to iterate up till the floored number
+    let sqrt_max_exclusive = number.isqrt() + 1;
+
+    // We start at 2 because we don't want the possible chunk size of the entire slice.
+    (2..sqrt_max_exclusive).for_each(|i| {
+        if number % i == 0 {
+            factors.push(i);
+
+            // Both the divisor and the quotient are factors, unless they are the sqrt
+            if i * i != number {
+                factors.push(number / i);
+            }
+        }
+    });
+
+    factors.sort();
+    factors
 }
