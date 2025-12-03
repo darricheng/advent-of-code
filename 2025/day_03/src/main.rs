1768266334e3613485d780fcc0fcfd1313968111
Darric Heng <darricheng@gmail.com>
2025-12-03 19:00:30 +0800

    solve 3-2

--- 2025/day_03/src/main.rs
+++ 2025/day_03/src/main.rs
@@ -9,14 +9,122 @@ fn main() {
     let data: Vec<&str> = contents.split('\n').collect();
 
     let result = data.iter().fold(0, |total, current_bank| {
-        total + get_largest_two_digit_number(&current_bank)
+        // 3-1
+        // total + get_largest_two_digit_number(&current_bank)
+
+        // 3-2
+        total + get_largest_twelve_digit_number(&current_bank)
     });
 
     println!("{}", result);
 }
 
+// This is the final byte slice
+struct Joltage([char; 12]);
+impl Joltage {
+    fn new(b: [char; 12]) -> Self {
+        Joltage(b)
+    }
+
+    // index: the index of the char to be compared
+    // new_values: the next slice of values
+    //
+    // The returned bool indicates whether the value at the
+    // current index was updated.
+    fn compare(self: &mut Self, index: usize, new_values: [char; 12]) -> bool {
+        let current_slice = self.0;
+
+        let current_value = current_slice
+            .get(index)
+            .expect("Failed to index into current_slice");
+        let new_value = new_values
+            .get(index)
+            .expect("Failed to index into new_values");
+
+        if new_value > current_value {
+            // Update the stored slice from the index to the end with the new_values
+            let current_sub_slice_to_keep = self
+                .0
+                .get(0..index)
+                .expect("Failed to get current_sub_slice_to_keep");
+            let new_sub_slice = new_values
+                .get(index..12)
+                .expect("Failed to get new_sub_slice");
+
+            self.0 = [current_sub_slice_to_keep, new_sub_slice]
+                .concat()
+                .try_into()
+                .expect("Failed to convert new slice into Joltage");
+
+            true
+        } else {
+            false
+        }
+    }
+
+    fn as_u64(self: &Self) -> u64 {
+        let num_str: String = self.0.iter().collect();
+        num_str
+            .parse::<u64>()
+            .expect("Failed to convert string to valid u64")
+    }
+}
+
+struct BankIter {
+    batteries: Vec<char>,
+    current_index: usize,
+    max_index: usize,
+}
+impl BankIter {
+    fn new(bank: &str) -> Self {
+        let batteries: Vec<char> = bank.chars().collect();
+        let max_index = batteries.len() - 12;
+
+        BankIter {
+            batteries,
+            current_index: 0,
+            // When current_index reaches max_index, that's the last value
+            max_index,
+        }
+    }
+}
+impl Iterator for BankIter {
+    type Item = [char; 12];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_index > self.max_index {
+            None
+        } else {
+            let start_index = self.current_index;
+            let end_index = start_index + 12;
+            let item = self
+                .batteries
+                .get(start_index..end_index)
+                .expect("Indexed out of range");
+
+            self.current_index += 1;
+
+            Some(item.try_into().expect("Didn't have 12 items to return"))
+        }
+    }
+}
+
 fn get_largest_twelve_digit_number(bank: &str) -> u64 {
-    0
+    let mut bank_iter = BankIter::new(bank);
+    let first_set = bank_iter
+        .next()
+        .expect("Couldn't get first_set from bank_iter");
+
+    let mut joltage = Joltage::new(first_set);
+
+    bank_iter.for_each(|slice| {
+        slice
+            .iter()
+            .enumerate()
+            .find(|(index, _)| joltage.compare(*index, slice));
+    });
+
+    joltage.as_u64()
 }
 
 /**
