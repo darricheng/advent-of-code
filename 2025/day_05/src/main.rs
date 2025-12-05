45b798b16988a92490f68e90f928ba15b0b44733
Darric Heng <darricheng@gmail.com>
2025-12-05 18:30:32 +0800

    solve 5-1 sample

---
+++ 2025/day_05/src/main.rs
@@ -0,0 +1,54 @@
+use std::fs;
+
+// Range(min, max)
+struct Range(u32, u32);
+impl Range {
+    fn new(range_str: &str) -> Self {
+        let mut iter = range_str.split('-');
+        let min = iter
+            .next()
+            .expect("Couldn't get min")
+            .parse()
+            .expect("Couldn't parse min");
+        let max = iter
+            .next()
+            .expect("Couldn't get max")
+            .parse()
+            .expect("Couldn't parse max");
+
+        Range(min, max)
+    }
+}
+
+fn main() {
+    let file_path = "sample.txt";
+
+    let binding = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents = binding.trim();
+
+    let data: Vec<&str> = contents.split('\n').collect();
+    let mut data_iter = data.split(|v| *v == "");
+
+    let ranges_strs = data_iter.next().expect("Couldn't get ranges_strs");
+    let number_strs = data_iter.next().expect("Couldn't get number_strs");
+
+    let ranges: Vec<Range> = ranges_strs
+        .iter()
+        .map(|range_str| Range::new(range_str))
+        .collect();
+
+    let result = number_strs.iter().fold(0, |total, num_str| {
+        let number: u32 = num_str.parse().expect("Couldn't parse into number");
+
+        if ranges
+            .iter()
+            .any(|Range(min, max)| number >= *min && number <= *max)
+        {
+            total + 1
+        } else {
+            total
+        }
+    });
+
+    println!("Result: {}", result);
+}
