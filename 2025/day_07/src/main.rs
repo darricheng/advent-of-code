1603d11c9e259754272a13c0fef396b4b2340cf4
Darric Heng <darricheng@gmail.com>
2025-12-10 09:08:14 +0800

    solve 7-2 sample

--- 2025/day_07/src/main.rs
+++ 2025/day_07/src/main.rs
@@ -27,13 +27,45 @@ fn part_one(mut rows_iter: Split<'_, char>) {
     println!("Result: {}", result);
 }
 
+fn part_two(mut rows_iter: Split<'_, char>) {
+    let first_row = rows_iter.next().expect("Should have first row");
+    let mut tachyon_beams_count: Vec<u32> = first_row
+        .chars()
+        .map(|char| if char == 'S' { 1 } else { 0 })
+        .collect();
+    let row_length = tachyon_beams_count.len();
+
+    rows_iter.for_each(|row| {
+        println!("Row: {:?}", row);
+        row.chars().enumerate().for_each(|(i, char)| {
+            if char == '^' && tachyon_beams_count[i] > 0 {
+                if i > 0 {
+                    tachyon_beams_count[i - 1] += tachyon_beams_count[i];
+                }
+                if i < row_length - 1 {
+                    tachyon_beams_count[i + 1] += tachyon_beams_count[i];
+                }
+                tachyon_beams_count[i] = 0;
+            }
+        });
+        println!("beam count: {:?}", tachyon_beams_count);
+    });
+
+    let result = tachyon_beams_count
+        .into_iter()
+        .reduce(|acc, curr| acc + curr)
+        .expect("Should have final u32 result");
+    println!("Result: {}", result);
+}
+
 fn main() {
-    let file_path = "data.txt";
+    let file_path = "sample.txt";
 
     let binding = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let contents = binding.trim_end_matches('\n');
 
     let rows_iter = contents.split('\n');
 
-    part_one(rows_iter);
+    // part_one(rows_iter);
+    part_two(rows_iter);
 }
